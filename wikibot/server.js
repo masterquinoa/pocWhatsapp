28c1365313c8014c5ad76ab6c2ac734c8407f11f
RicMartinez <ric.martinez.castro@gmail.com>
2019-11-06 16:40:36 +0100

    minor

--- wikibot/server.js
+++ wikibot/server.js
@@ -13,40 +13,43 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/incoming', (req, res) => {
-  const twiml = new MessagingResponse();
-  if (
-    req.body.Body.toLowerCase().trim() != "hi" &&
-    req.body.Body.toLowerCase().trim() != "hello" &&
-    req.body.Body.toLowerCase().trim() != "test" &&
-    req.body.Body.toLowerCase().trim() != "help") {
-    request(
-      'https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q=' +
-      req.body.Body, function (error, response, body) {
-        body = JSON.parse(body)
-        console.log('body:', body["Abstract"]);
+app.post('/incoming', async (req, res) => {
+  try {
+    const twiml = new MessagingResponse();
+    if (
+      req.body.Body.toLowerCase().trim() != "hi" &&
+      req.body.Body.toLowerCase().trim() != "hello" &&
+      req.body.Body.toLowerCase().trim() != "test" &&
+      req.body.Body.toLowerCase().trim() != "help") {
+      request(
+        'https://api.duckduckgo.com/?skip_disambig=1&format=json&pretty=1&q=' +
+        req.body.Body, function (error, response, body) {
+          body = JSON.parse(body)
+          console.log('body:', body["Abstract"]);
 
-        if (body["Abstract"] == "") {
-          body["Abstract"] = body["RelatedTopics"][0]["Text"]
-        }
+          if (body["Abstract"] == "") {
+            body["Abstract"] = body["RelatedTopics"][0]["Text"]
+          }
 
-        var msg = twiml.message(`*` + body["Heading"] + `*
+          var msg = twiml.message(`*` + body["Heading"] + `*
 
 `   + body["Abstract"]);
-        res.writeHead(200, { 'Content-Type': 'text/xml' });
-        res.end(twiml.toString());
-      });
-  }
-  else {
-    var msg = twiml.message(`*Hey 👋*
+          res.writeHead(200, { 'Content-Type': 'text/xml' });
+          res.end(twiml.toString());
+        });
+    }
+    else {
+      var msg = twiml.message(`*Hey 👋*
 
           I am a bot which summarizes WikiPedia pages to help you find quick information, right within WhatsApp.
 
           Try it out - send me anything you want to know about`)
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.end(twiml.toString());
+    }
+  } catch (e) {
+    console.log(e)
   }
-
 });
 
 app.post('/check', function (req, res) {
